c4e020682113711d393320b519b9814120380453
borgepl <pdeborger@gmail.com>
2022-09-20 15:46:41 +0200

    More Material Form Fields

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {UntypedFormBuilder, Validators} from '@angular/forms';
+import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
 
+const SAMPLE_TEXT = "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 @Component({
   selector: "create-course-step-1",
@@ -19,11 +21,42 @@ export class CreateCourseStep1Component {
     category: ['BEGINNER', Validators.required],
     courseType: ['premium', Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
-    longDescription: ['', [Validators.required, Validators.minLength(3)]]
+    longDescription: [SAMPLE_TEXT, [Validators.required, Validators.minLength(3)]]
   });
 
-  constructor(private fb: UntypedFormBuilder) {
+  holidays = [
+    { date: "2022-10-02" },
+    { date: "2022-10-03" },
+    { date: "2022-10-14" },
+    { date: "2022-10-05" },
+    { date: "2022-10-06" }
+  ];
 
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+     // Only highligh dates inside the month view.
+     if (view === 'month') {
+      const date = cellDate.getDate();
+      const dateSearch = this.dateToString(cellDate);
+      // Highlight the 1st and 15th day of each month.
+      // return date === 1 || date === 15 ? 'highlight-date' : '';
+      // Highlight Holidays
+      return this.holidays.find(f => f.date == dateSearch) || date === 1 || date === 15
+      ? "highlight-date" : undefined;
+    }
+    return '';
+  };
+
+
+  constructor(private fb: UntypedFormBuilder) {}
+
+  dateToString(date: any) {
+    return (
+      date.getFullYear() +
+      "-" +
+      ("0" + (date.getMonth() + 1)).slice(-2) +
+      "-" +
+      ("0" + date.getDate()).slice(-2)
+    );
   }
 
   get courseTitle() {
