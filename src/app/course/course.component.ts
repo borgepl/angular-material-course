1f803e3498885bfc0cc6174446413f96b3f90b34
borgepl <pdeborger@gmail.com>
2022-09-21 16:17:02 +0200

    Data Table with pagination and sort

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,12 +1,13 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import { MatPaginator } from "@angular/material/paginator";
 import { MatSort } from "@angular/material/sort";
 import { MatTableDataSource } from "@angular/material/table";
 import {Course} from "../model/course";
 import {CoursesService} from "../services/courses.service";
-import {debounceTime, distinctUntilChanged, startWith, tap, delay} from 'rxjs/operators';
-import {merge, fromEvent} from "rxjs";
+import {debounceTime, distinctUntilChanged, startWith, tap, delay } from 'rxjs/operators';
+import {merge, fromEvent, throwError, Subscription} from "rxjs";
+import { Lesson } from '../model/lesson';
 
 
 @Component({
@@ -14,11 +15,13 @@ import {merge, fromEvent} from "rxjs";
     templateUrl: './course.component.html',
     styleUrls: ['./course.component.scss']
 })
-export class CourseComponent implements OnInit, AfterViewInit {
+export class CourseComponent implements OnInit, AfterViewInit, OnDestroy {
 
     course:Course;
+    subscription: Subscription;
+    loading: boolean = false;
 
-    lessons = [
+/*     lessons = [
        {
         id: 120,
         'description': 'Introduction to Angular Material',
@@ -96,23 +99,81 @@ export class CourseComponent implements OnInit, AfterViewInit {
         'seqNo': 11,
         courseId: 11
       }
-    ];
+    ]; */
 
-    constructor(private route: ActivatedRoute,
-                private coursesService: CoursesService) {
+    lessons: Lesson[] = [];
+
+    @ViewChild(MatPaginator)
+    paginator: MatPaginator;
+
+    @ViewChild(MatSort)
+    sort: MatSort;
+
+    constructor( private route: ActivatedRoute,
+                 private coursesService: CoursesService) {
 
     }
+
+
+    displayedColumns = ['seqNo','description','duration'];
 
     ngOnInit() {
 
         this.course = this.route.snapshot.data["course"];
-
+        this.loadLessonsPage();
 
     }
 
+  loadLessonsPage() {
+ /*    this.coursesService.findLessons(this.course.id,"asc",0,3)
+      .pipe(
+        tap(lessons => this.lessons = lessons),
+        catchError(err => {
+          console.log('Error loading lessons: ', err);
+          alert('Error loading lessons');
+          return throwError(err);
+        })
+      )
+      .subscribe(); */
+
+    this.loading = true;
+
+    this.subscription =
+       this.coursesService.findLessons(
+          this.course.id,
+          this.sort?.direction ?? "asc",
+          this.paginator?.pageIndex ?? 0,
+          this.paginator?.pageSize ?? 3,
+          this.sort?.active ?? "seqNo"
+          )
+        .subscribe({
+        next: (lessons) =>  {
+          this.lessons = lessons;
+          this.loading = false;
+        },
+        error: (err) => {
+          console.log('Error loading lessons: ' + err);
+          alert('Error loading lessons');
+          this.loading = false;
+        }
+       });
+  }
+
     ngAfterViewInit() {
 
+      this.sort.sortChange.subscribe(
+        () => this.paginator.pageIndex = 0
+      );
 
+      merge(this.sort.sortChange, this.paginator.page)
+        .pipe(
+          tap(() => this.loadLessonsPage())
+        )
+        .subscribe();
+    }
+
+    ngOnDestroy(): void {
+      this.subscription.unsubscribe();
     }
 
 }
